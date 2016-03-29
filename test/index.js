31ec87d7bda1feffe18927d74c4fadaa087c80a6
Max Joehnk <maxjoehnk@gmail.com>
2016-03-29 12:01:20 +0200

    Add more tests

--- test/index.js
+++ test/index.js
@@ -1,6 +1,11 @@
-const chai = require('chai');
-const sinon = require('sinon');
-const sinonChai = require('sinon-chai');
+const chai          = require('chai');
+const sinon         = require('sinon');
+const sinonChai     = require('sinon-chai');
+
+const Server        = require('socket.io');
+const Client        = require('socket.io-client');
+const http          = require('http');
+
 chai.use(sinonChai);
 
 var expect = chai.expect;
@@ -9,70 +14,142 @@ var plugin = require('../lib');
 
 describe('mongoose-socket.io-events', function() {
     var schema;
+    var options;
+    var io;
+    var server;
+    var callbacks;
 
     beforeEach(function() {
+        callbacks = {};
+        server = http.createServer().listen(8090);
         schema = {
-            post: sinon.spy()
+            post: function(name, cb) {
+                callbacks[name] = cb.bind({
+                    _id: 'stubid'
+                });
+            }
         };
+        io = new Server(server);
+        options = {
+            attach: server,
+            room: false
+        };
+
     });
 
-    it('should call schema.post on default hooks', function() {
-        plugin(schema);
+    afterEach(function() {
+        server.close();
+    });
+
+    it('should call schema.post on default hooks', sinon.test(function() {
+        this.spy(schema, 'post');
+        plugin(schema, options);
         expect(schema.post).to.have.been.calledWith('save');
         expect(schema.post).to.have.been.calledWith('init');
         expect(schema.post).to.have.been.calledWith('remove');
         expect(schema.post).to.have.been.calledThrice;
-    });
+    }));
 
-    it('should call schema.post only on save hook', function() {
-        plugin(schema, {
-            events: {
-                save: true,
-                init: false,
-                remove: false,
-                validate: false
-            }
-        });
+    it('should call schema.post only on save hook', sinon.test(function() {
+        this.spy(schema, 'post');
+        options.events = {
+            save: true,
+            init: false,
+            remove: false,
+            validate: false
+        };
+        plugin(schema, options);
         expect(schema.post).to.have.been.calledWith('save');
         expect(schema.post).to.have.been.calledOnce;
-    });
+    }));
 
-    it('should call schema.post only on init hook', function() {
-        plugin(schema, {
-            events: {
-                save: false,
-                init: true,
-                remove: false,
-                validate: false
-            }
-        });
+    it('should call schema.post only on init hook', sinon.test(function() {
+        this.spy(schema, 'post');
+        options.events = {
+            save: false,
+            init: true,
+            remove: false,
+            validate: false
+        };
+        plugin(schema, options);
         expect(schema.post).to.have.been.calledWith('init');
         expect(schema.post).to.have.been.calledOnce;
-    });
+    }));
 
-    it('should call schema.post only on remove hook', function() {
-        plugin(schema, {
-            events: {
-                save: false,
-                init: false,
-                remove: true,
-                validate: false
-            }
-        });
+    it('should call schema.post only on remove hook', sinon.test(function() {
+        this.spy(schema, 'post');
+        options.events = {
+            save: false,
+            init: false,
+            remove: true,
+            validate: false
+        };
+        plugin(schema, options);
         expect(schema.post).to.have.been.calledWith('remove');
         expect(schema.post).to.have.been.calledOnce;
-    });
+    }));
 
-    it('should call schema.post only on validate hook', function() {
-        plugin(schema, {
-            events: {
-                save: false,
-                init: false,
-                remove: false,
-                validate: true
-            }
-        });
+    it('should call schema.post only on validate hook', sinon.test(function() {
+        this.spy(schema, 'post');
+        options.events = {
+            save: false,
+            init: false,
+            remove: false,
+            validate: true
+        };
+        plugin(schema, options);
         expect(schema.post).to.have.been.calledWith('validate');
         expect(schema.post).to.have.been.calledOnce;
+    }));
+
+    it('should emit a save event', function(done) {
+        var connection = Client('http://localhost:8090');
+        connection.on('connect', function() {
+            connection.on('mongoose:save', function(payload) {
+                expect(payload).to.deep.equal({id: 'stubid'});
+                done();
+            });
+            callbacks.save(function() {});
+        });
+        plugin(schema, options);
+    });
+
+    it('should emit a init event', function(done) {
+        var connection = Client('http://localhost:8090');
+        connection.on('connect', function() {
+            connection.on('mongoose:init', function(payload) {
+                expect(payload).to.deep.equal({id: 'stubid'});
+                done();
+            });
+            callbacks.init(function() {});
+        });
+        plugin(schema, options);
+    });
+
+    it('should emit a remove event', function(done) {
+        var connection = Client('http://localhost:8090');
+        connection.on('connect', function() {
+            connection.on('mongoose:remove', function(payload) {
+                expect(payload).to.deep.equal({id: 'stubid'});
+                done();
+            });
+            callbacks.remove(function() {});
+        });
+        plugin(schema, options);
+    });
+
+    it('should emit a validate event', function(done) {
+        var connection = Client('http://localhost:8090');
+        connection.on('connect', function() {
+            connection.on('mongoose:validate', function(payload) {
+                expect(payload).to.deep.equal({id: 'stubid'});
+                done();
+            });
+            callbacks.validate(function() {});
+        });
+        options.events = {
+            validate: true
+        };
+        plugin(schema, options);
     });
 });
